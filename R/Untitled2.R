581eb9550e90bb430d48a3b977ded922bc9bf6cb
scottiejj <raozx@outlook.com>
2024-11-19 18:11:50 -0600

    Add new file to R folder

---
+++ R/Untitled2.R
@@ -0,0 +1,14 @@
+#' Add Two Numbers
+#'
+#' This function takes two numeric inputs and returns their sum.
+#'
+#' @param a A numeric value.
+#' @param b A numeric value.
+#' @return The sum of \code{a} and \code{b}.
+#' @examples
+#' add_numbers(5, 3)
+#' add_numbers(-2, 10)
+#' @export
+add_numbers <- function(a, b) {
+  return(a + b)
+}
